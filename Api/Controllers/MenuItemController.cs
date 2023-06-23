63f3cd6772219e2f4cd60fe17a28c98fbd251632
codeinsack <codeinsack@gmail.com>
2023-06-23 23:34:06 +0200

    implement crud of MenuItem

--- Api/Controllers/MenuItemController.cs
+++ Api/Controllers/MenuItemController.cs
@@ -1,6 +1,7 @@
 using System.Net;
 using Api.Data;
 using Api.Models;
+using Api.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers;
@@ -26,7 +27,7 @@ public class MenuItemController : ControllerBase
         return Ok(_response);
     }
 
-    [HttpGet("id:int")]
+    [HttpGet("{id:int}", Name = "GetMenuItem")]
     public async Task<IActionResult> GetMenuItem(int id)
     {
         if (id == 0)
@@ -48,5 +49,137 @@ public class MenuItemController : ControllerBase
         _response.Result = menuItem;
         _response.StatusCode = HttpStatusCode.OK;
         return Ok(_response);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ApiResponse>> CreateMenuItem([FromBody]MenuItemCreateDto menuItemCreateDto)
+    {
+        try
+        {
+            if (ModelState.IsValid)
+            {
+                var menuItemToCreate = new MenuItem()
+                {
+                    Name = menuItemCreateDto.Name,
+                    Category = menuItemCreateDto.Category,
+                    Description = menuItemCreateDto.Description,
+                    Price = menuItemCreateDto.Price,
+                    Image = menuItemCreateDto.Image,
+                    SpecialTag = menuItemCreateDto.SpecialTag,
+                };
+
+                _db.MenuItems.Add(menuItemToCreate);
+
+                _db.SaveChanges();
+                _response.Result = menuItemToCreate;
+                _response.StatusCode = HttpStatusCode.OK;
+                return CreatedAtRoute("GetMenuItem", new
+                {
+                    id = menuItemToCreate.Id
+                }, _response);
+            }
+            else
+            {
+                _response.IsSuccess = false;
+            }
+        }
+        catch (Exception ex)
+        {
+            _response.IsSuccess = false;
+            _response.ErrorMessages = new List<string>()
+            {
+                ex.ToString()
+            };
+        }
+
+        return _response;
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<ActionResult<ApiResponse>> UpdateMenuItem(
+        int id,
+        [FromBody]MenuItemUpdateDto menuItemUpdateDto
+    )
+    {
+        try
+        {
+            if (ModelState.IsValid)
+            {
+                if (menuItemUpdateDto == null || id == 0)
+                {
+                    return BadRequest();
+                }
+
+                var menuItemFromDb = await _db.MenuItems.FindAsync(id);
+
+                if (menuItemFromDb == null)
+                {
+                    return BadRequest();
+                }
+
+                menuItemFromDb.Name = menuItemUpdateDto.Name;
+                menuItemFromDb.Price = menuItemUpdateDto.Price;
+                menuItemFromDb.Category = menuItemUpdateDto.Category;
+                menuItemFromDb.SpecialTag = menuItemUpdateDto.SpecialTag;
+                menuItemFromDb.Description = menuItemUpdateDto.Description;
+
+                _db.MenuItems.Update(menuItemFromDb);
+
+                _db.SaveChanges();
+                _response.Result = menuItemFromDb;
+                _response.StatusCode = HttpStatusCode.NoContent;
+                return Ok(_response);
+            }
+            else
+            {
+                _response.IsSuccess = false;
+            }
+        }
+        catch (Exception ex)
+        {
+            _response.IsSuccess = false;
+            _response.ErrorMessages = new List<string>()
+            {
+                ex.ToString()
+            };
+        }
+
+        return _response;
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<ActionResult<ApiResponse>> DeleteMenuItem(int id)
+    {
+        try
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+
+            var menuItemFromDb = await _db.MenuItems.FindAsync(id);
+
+            if (menuItemFromDb == null)
+            {
+                return BadRequest();
+            }
+
+            _db.MenuItems.Remove(menuItemFromDb);
+
+            _db.SaveChanges();
+            _response.Result = menuItemFromDb;
+            _response.StatusCode = HttpStatusCode.NoContent;
+            return Ok(_response);
+        }
+        catch (Exception ex)
+        {
+            _response.IsSuccess = false;
+            _response.ErrorMessages = new List<string>()
+            {
+                ex.ToString()
+            };
+        }
+
+        return _response;
     }
 }
---
+++ Api/Models/Dto/MenuItemUpdateDto.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.Models.Dto;
+
+public class MenuItemUpdateDto
+{
+    [Required]
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+    public string SpecialTag { get; set; }
+    public string Category { get; set; }
+
+    [Range(1, int.MaxValue)]
+    public double Price { get; set; }
+
+    [Required]
+    public string Image { get; set; }
+}
